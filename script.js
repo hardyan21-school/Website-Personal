86e175a5e64232a99c6dd8ebc0d9fa4f7a53778b
hardyan21-school <hardyanwijaya7@gmail.com>
2025-02-06 21:33:20 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Menambahkan event listener untuk setiap anggota tim
-document.querySelectorAll('.team-member').forEach(member => {
-  member.addEventListener('click', function() {
-    // Toggle class active saat foto anggota diklik
-    this.classList.toggle('active');
-  });
-});
